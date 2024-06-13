546c600ac4a7610d72548fd6302cc506d1ed8377
Pax Kim <kimpaxpax@gmail.com>
2024-06-12 22:56:53 -0400

    Dash
    Me - - - - - - > Me

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,33 +10,62 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 moveVector = Vector2.zero;
     private Rigidbody2D rb = null;
 
-    [SerializeField]
-    private float moveSpeed = 10;
+    [SerializeField] private float moveSpeed = 10f;
+
+    [Header("Dashing")]
+    [SerializeField] private float dashingVelocity = 20f;
+    [SerializeField] private float dashingTime = 0.15f;
+    [SerializeField] private bool isDashing;
+    [SerializeField] private bool canDash = true;
+    [SerializeField] private float dashCooldown = 3f; // Cooldown time between dashes
 
     private void Awake() {
         input = new CustomInput();
         rb = GetComponent<Rigidbody2D>();
+
     }
     private void OnEnable() {
         input.Enable();
         input.Player.Movement.performed += OnMovementPerformed;
         input.Player.Movement.canceled += OnMovementCancelled;
+        input.Player.Dash.performed += OnDashPerformed; 
     }
 
     private void OnDisable() {
         input.Disable();
         input.Player.Movement.performed -= OnMovementPerformed;
         input.Player.Movement.canceled -= OnMovementCancelled;
+        input.Player.Dash.performed -= OnDashPerformed;
     }
     
     private void FixedUpdate() {
-        // Debug.Log(moveVector);
-        rb.velocity = moveVector * moveSpeed;
+        if (!isDashing) {
+            rb.velocity = moveVector * moveSpeed;
+        }
     }    
     private void OnMovementPerformed(InputAction.CallbackContext value){
         moveVector = value.ReadValue<Vector2>();
     }
     private void OnMovementCancelled(InputAction.CallbackContext value){
         moveVector = Vector2.zero;
+    }
+    private void OnDashPerformed(InputAction.CallbackContext value){
+        if (canDash && !isDashing){
+            StartCoroutine(Dash());
+        }
+    }
+    private IEnumerator Dash(){
+        isDashing = true;
+        canDash = false;
+        Debug.Log("Can't Dash");
+        Vector2 originalVelocity = rb.velocity;
+        rb.velocity = moveVector.normalized * dashingVelocity;
+        yield return new WaitForSeconds(dashingTime);
+        rb.velocity = originalVelocity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+
+        canDash = true;
+        Debug.Log("Can Dash");
     }
 }
